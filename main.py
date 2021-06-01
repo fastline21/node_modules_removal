03e68fd8bbb44d6074bf06fefb37e52de09760b9
Joshwa Facistol <fastline2123@gmail.com>
2021-06-02 04:48:16 +0800

    added pipenv and json export

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import os
 import shutil
+import json
+from datetime import datetime
+from tqdm import tqdm
 
 current_directory = input("Enter directory: ")
 
 result = os.listdir(current_directory)
-count = 0
+result = []
+today = datetime.now()
+
 for root, dirs, files in os.walk(current_directory):
-    for dir in dirs:
+    for dir in tqdm(dirs):
         if dir == 'node_modules':
             current_node_modules_path = os.path.join(root, dir)
             if os.path.exists(current_node_modules_path) and os.path.isdir(current_node_modules_path):
                 shutil.rmtree(current_node_modules_path, ignore_errors=True)
-            count = + 1
+                result.append(current_node_modules_path)
 
-print(f"You removed {count} node_modules")
+result_body = {
+    "directory": current_directory,
+    "count": len(result),
+    "date": today.strftime("%B %d, %Y - %I:%M:%S %p"),
+    "list": result
+}
+
+if not os.path.exists("result"):
+    os.makedirs("result")
+
+with open(f"result/data_{today.strftime('%m-%d-%Y-%I-%M-%S-%p')}.json", "w") as new_json:
+    json.dump(result_body, new_json)
+
+print(f"You removed {len(result)} node_modules folder")
